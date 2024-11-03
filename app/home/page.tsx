89d618ee24b5d4941c1aefed7ea3252c03c26ea3
Nekruz Avgani <nekruz.avgani@gmail.com>
2024-11-03 08:52:41 +0000

    add hotjar for analytics

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -13,70 +13,49 @@ import StudentRanking from "../components/dashboard/StudentRanking"
 import DashboardTopics from "../components/dashboard/DashboardTopics"
 import DashboardAvatar from "@/components/dashboard/DashboardAvatar"
 import PricingModal from "@/components/pricing/PricingModal"
-import Head from "next/head"
-import Script from "next/dist/client/script"
 
 function MyDashboard() {
 	const { userInfo } = useStoreUser()
 
 	return (
-		<>
-			<Head>
-				<Script
-					id="hotjar"
-					strategy="afterInteractive"
-					dangerouslySetInnerHTML={{
-						__html: `
-            (function(c,s,q,u,a,r,e){
-              c.hj=c.hj||function(){(c.hj.q=c.hj.q||[]).push(arguments)};
-              c._hjSettings={hjid:5192862};
-              r=s.getElementsByTagName('head')[0];
-              e=s.createElement('script');e.async=1;
-              e.src=q+c._hjSettings.hjid+u;r.appendChild(e);
-            })(window,document,'https://static.hj.contentsquare.net/c/csq-','.js');
-          `
-					}}
-				/>
-			</Head>
-			<ProtectedRoute permitArray={["admin", "student"]}>
-				<SidebarContainer>
-					<Grid container spacing={2}>
-						<Grid item xs={12} md={6}>
-							{/* @ts-ignore */}
-							<Box display={["flex", "none"]} alignItems="center" mb={2}>
-								{((userInfo?.role === "student" && !userInfo?.paid) || userInfo?.role === "admin") && (
-									<PricingModal buttonText="Upgrade to Pro" />
-								)}
-							</Box>
-							<DashboardLessonTimetable />
-							<DashboardLessonTimetableMobile />
-						</Grid>
-						<Grid item xs={12} md={6}>
-							<DashboardAvatar />
-						</Grid>
-						<Grid item xs={12} sm={8}>
-							<Grid container spacing={2}>
-								<Grid item xs={12}>
-									<ExploreTopics />
-								</Grid>
-								<Grid item xs={12} sm={6}>
-									<WordOfTheDay />
-								</Grid>
-								<Grid item xs={12} sm={6}>
-									<MostRecentTestScore />
-								</Grid>
+		<ProtectedRoute permitArray={["admin", "student"]}>
+			<SidebarContainer>
+				<Grid container spacing={2}>
+					<Grid item xs={12} md={6}>
+						{/* @ts-ignore */}
+						<Box display={["flex", "none"]} alignItems="center" mb={2}>
+							{((userInfo?.role === "student" && !userInfo?.paid) || userInfo?.role === "admin") && (
+								<PricingModal buttonText="Upgrade to Pro" />
+							)}
+						</Box>
+						<DashboardLessonTimetable />
+						<DashboardLessonTimetableMobile />
+					</Grid>
+					<Grid item xs={12} md={6}>
+						<DashboardAvatar />
+					</Grid>
+					<Grid item xs={12} sm={8}>
+						<Grid container spacing={2}>
+							<Grid item xs={12}>
+								<ExploreTopics />
+							</Grid>
+							<Grid item xs={12} sm={6}>
+								<WordOfTheDay />
+							</Grid>
+							<Grid item xs={12} sm={6}>
+								<MostRecentTestScore />
 							</Grid>
 						</Grid>
-						<Grid item xs={12} sm={4}>
-							<StudentRanking />
-						</Grid>
-						<Grid item xs={12}>
-							<DashboardTopics />
-						</Grid>
 					</Grid>
-				</SidebarContainer>
-			</ProtectedRoute>
-		</>
+					<Grid item xs={12} sm={4}>
+						<StudentRanking />
+					</Grid>
+					<Grid item xs={12}>
+						<DashboardTopics />
+					</Grid>
+				</Grid>
+			</SidebarContainer>
+		</ProtectedRoute>
 	)
 }
 
